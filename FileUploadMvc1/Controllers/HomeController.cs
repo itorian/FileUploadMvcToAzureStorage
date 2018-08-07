ce477f8f5b7b71711875edae31a84db3ba40bab3
Abhimanyu Kumar Vatsa <abhimanyu@knorish.com>
2018-08-07 16:56:57 +0530

    Add project files.

---
+++ FileUploadMvc1/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace FileUploadMvc1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ FileUploadMvc1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileUploadMvc1.MvcApplication" Language="C#" %>
